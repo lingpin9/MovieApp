2863b4fd00885804f0c2a79a093ca9dfb8d6b37b
Ling Pin <lingpin09@gmail.com>
2021-07-20 20:16:15 +0800

    Pulling images from API

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -1,7 +1,7 @@
 <template>
-  <q-card>
+  <q-card class="card">
     <q-card-section>
-      <div class="text-h6 card">
+      <div class="text-h6">
         Search Movie By Title
       </div>
     </q-card-section>
@@ -19,14 +19,17 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { API_KEY } from 'src/service/constants';
+import { MovieResponse, ImageResponse, Image } from 'src/service/models';
 
 export default defineComponent({
   // name: 'ComponentName',
   setup () {
     const searchModel = ref('');
-
+    const listOfMovies = ref<AxiosResponse<MovieResponse>>();
+    const listOfMovieImages = ref<AxiosResponse<ImageResponse>>();
+    const listOfMovieImageLinks = ref<string[]>([]);
 
     return {
       search,
@@ -34,9 +37,30 @@ export default defineComponent({
     }
 
     async function search() {
-      const temp = searchModel.value.replace(/ /g, '%20');
-      const result = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${temp}`)
-      console.log(result);
+      if (searchModel.value != '') {
+        const temp = searchModel.value.replace(/ /g, '%20');
+        listOfMovies.value = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${temp}`);
+        console.log(listOfMovies.value?.data.results);
+        await getImages();
+      } else console.log('Nothing')
+    }
+
+    async function getImages() {
+      // debugger;
+      const movieIds: number[] = [];
+      listOfMovies.value?.data.results.forEach(x => movieIds.push(x.id))
+      console.log(movieIds);
+      for (let i = 0; i < movieIds.length; i++) {
+        console.log(movieIds[i]);
+        const temp:AxiosResponse<ImageResponse> = await axios.get(`https://api.themoviedb.org/3/movie/${movieIds[i]}/images?api_key=${API_KEY}`);
+
+        if(temp.data.posters.length > 0) {
+          console.log(temp.data.posters[0])
+          listOfMovieImageLinks.value.push(temp.data.posters[0].file_path);
+        } else listOfMovieImageLinks.value.push('');
+
+      }
+      console.log(listOfMovieImageLinks.value);
     }
   }
 });
--- src/service/models.ts
+++ src/service/models.ts
@@ -5,3 +5,42 @@ export interface MovieDetails {
   imageLink: string;
   overview: string;
 }
+
+export interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_results: number;
+  total_pages: number;
+}
+
+export interface Movie {
+  poster_path: string | null;
+  adult: boolean;
+  overview: string;
+  release_date: string;
+  genre_ids: string;
+  id: number;
+  original_title: string;
+  original_language: string;
+  title: string;
+  backdrop_path: string | null;
+  popularity: number;
+  vote_count: number;
+  video: boolean;
+  vote_average: number;
+}
+
+export interface Image {
+  aspect_ratio: number;
+  height: number;
+  iso_639_1: string | null;
+  file_path: string;
+  vote_average: number;
+  vote_count: number;
+  width: number;
+}
+
+export interface ImageResponse {
+  backdrops: Image[];
+  posters: Image[];
+}
